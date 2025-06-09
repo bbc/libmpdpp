003b487c1320e4e03c7b62a74f217de6b5df0ef2
deva <deva@rd.bbc.co.uk>
2025-06-09 18:00:10 +0100

    Mark ContentComponent header file for documentation

--- include/libmpd++/ContentComponent.hh
+++ include/libmpd++/ContentComponent.hh
@@ -28,18 +28,59 @@ LIBMPDPP_NAMESPACE_BEGIN
 
 class AdaptationSet;
 
+/**
+ * @brief ContentComponent class
+ * @headerfile libmpd++/ContentComponent.hh <libmpd++/ContentComponent.hh>
+ *
+ * This is a container to hold the properties of a media content component described by ContentComponent element as defined in ISO 23009-1:2022.
+ *
+ */
 class LIBMPDPP_PUBLIC_API ContentComponent {
 public:
+    /** Default constructor
+     *
+     * Creates an empty ContentComponent.
+     */
     ContentComponent() {};
-
+    
+    /** Destructor
+     */
     virtual ~ContentComponent() {};
 
-    bool operator==(const ContentComponent&) const { return true; };
+    /** Equality operator
+     *
+     * @brief Compares this ContentComponent with another for equality.
+     * This operator overload allows checking if two ContentComponent instances are equal.
+     * Currently, the implementation always returns true, meaning all instances are considered equal,
+     * regardless of their internal state.
+     * @param to_compare A constant reference to the ContentComponent instance to compare against.
+     * @return `true` if the components are considered equal, false otherwise..
+     */
+    bool operator==(const ContentComponent &to_compare) const { return true; };
 
+///@cond PROTECTED
 protected:
     friend class AdaptationSet;
+
+    /**
+     * XML constructor (internal use only)
+     *
+     * Create a new ContentComponent from an XML %ContentComponent element.
+     *
+     * @param node The libxml++ Node for the %ContentComponent element contained in Adaptation Set.
+     */
     ContentComponent(xmlpp::Node&);
+
+    /**
+     * Set a libxml++ Element attributes and children for XML output
+     *
+     * This will set all relevant attributes and child elements from the settings of this ContentComponent.
+     *
+     * @param element The libxml++ Element to populate.
+     */
     void setXMLElement(xmlpp::Element&) const;
+
+///@endcond PROTECTED
 
 private:
 };
